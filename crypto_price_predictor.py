c1ec52f07244cbb7f3956df7335f88e1181bb529
Tristan Larsin <tlarsin@gmail.com>
2018-02-21 18:23:56 -0600

    Added basic Class structure for main running of file.

---
+++ crypto_price_predictor.py
@@ -0,0 +1,10 @@
+
+class CryptoPricePredictor(object):
+    """docstring for CryptoPricePredictor."""
+    def __init__(self):
+        super(CryptoPricePredictor, self).__init__()
+
+
+
+if __name__ == '__main__':
+    CryptoPricePredictor()
