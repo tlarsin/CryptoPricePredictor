8df9bc910cba66d1595ceb6d0901be7ad1f5905b
Ben Carpenter <carpe504@d.umn.edu>
2018-02-22 19:52:53 -0600

    added progress report one as a latex file

--- crypto_price_predictor.py
+++ crypto_price_predictor.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 
 class CryptoPricePredictor(object):
     """docstring for CryptoPricePredictor."""
@@ -8,3 +10,10 @@ class CryptoPricePredictor(object):
 
 if __name__ == '__main__':
     CryptoPricePredictor()
+
+def read_data(uri):
+    """Given a URI, return processed data"""
+    df = pd.read_csv(uri)
+    # do additional data processing here
+
+
