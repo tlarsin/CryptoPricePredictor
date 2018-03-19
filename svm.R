55e0f0352fd1b56934fcf709c2ee355cadcc061b
Tristan Larsin <tlarsin@gmail.com>
2018-03-19 16:09:46 -0500

    Updated the SVM function to grab the features & labels of the training and test sets. I also added two new function stubs: lagrange_multipliers() and gram_matrix().

--- svm.R
+++ svm.R
@@ -3,10 +3,34 @@
 # Support Vector Model
 # Parameters: train (Training data - 80%), test(Testing data - 20%)
 SVM = function(train, test) {
+	# Features -- BTC Volume in Currency
+	train.X = train[6]
+	test.X = test[6]
+
+	# Labels -- BTC Open Value
+	train.y = train[1]
+	test.y = test[1]
 
 }
 
-ReadData = function() {
+# Computes Lagrange multipliers
+# Returns Lagrange multipliers
+lagrange_multipliers = function(X, y) {
+	K = gram_matrix(X)
+
+}
+
+# Gram Matrix
+# Returns the Gram Matrix
+gram_matrix = function(X) {
+
+
+}
+
+
+# Reads data from .csv file and splits it into
+# training/testing sets
+read_data = function() {
 	print('Reading data...')
 
 	# Bitcoin dataset at 1-minute interval Jan 2012 - Jan 2018
@@ -26,4 +50,4 @@ ReadData = function() {
 }
 
 # Calls ReadData
-ReadData()
+read_data()
